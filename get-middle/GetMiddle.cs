f6e10e3c3e60267fb1dcd3c775f66305f94f165e
rawabet <rawabe.alshduki@tuwaiq.edu.sa>
2021-04-21 17:02:34 +0300

    Add get middle By Rawabe

---
+++ get-middle/GetMiddle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GetMiddle
+{
+    class Program
+    {
+        public static void GetMiddle(string word)
+        {
+            if (word.Length % 2 == 0)
+                Console.WriteLine(word.Substring((word.Length - 1) / 2, 2));
+            else
+                Console.WriteLine(word.Length / 2);
+        }
+
+        static void Main(string[] args)
+        {
+            GetMiddle("rawabe");
+        }
+    }
+}
