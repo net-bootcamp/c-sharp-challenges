d114d3b78947bb635e6a18de15b59198189c6bb3
Saud <saud.alshammari@tuwaiq.edu.sa>
2021-04-21 15:28:33 +0300

    add: isBetween method

---
+++ check-letter-between/isBetween.cs
@@ -0,0 +1,9 @@
+public static bool isbetween(char targetValue,char fisrtValue, char lastValue)
+        {
+
+            if (targetValue>=fisrtValue&&targetValue<=lastValue)
+            {
+                return true;
+            }
+            return false;
+        }
