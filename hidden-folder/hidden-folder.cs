3e1bcebb5a0020372b723749469a47f6fa093547
AlbandryAlQaseemi <82465813+AlbandryAlQaseemi@users.noreply.github.com>
2021-04-21 14:18:21 +0300

    Add files via upload

---
+++ hidden-folder/hidden-folder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace hidden_folder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write your folder name: ");
+            string file = Console.ReadLine();
+            if (file.StartsWith('.'))
+            {
+                Console.WriteLine("This is a hidden folder");
+            }
+            else
+            {
+
+                Console.WriteLine("Your folder is not a hidden folder");
+            }
+        }
+    }
+}
