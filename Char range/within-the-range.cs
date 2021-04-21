b126177f08c20376ec17da23d1a277308d3418bd
mohammed-tu <82476026+mohammed-tu@users.noreply.github.com>
2021-04-21 16:16:05 +0300

    from Mohammed

---
+++ Char range/within-the-range.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp_4_211_v2
+{
+    class Program
+    {
+        static bool withinTheScop(char a, char b, char c)
+        {
+            if (b >= a && b <= c)
+                return true;
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(withinTheScop('z', 'a', 'c'));
+        }
+
+    }
+}
