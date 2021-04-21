442010449dbce8ccfba7332354cdacac8b1f6556
aalhaif <Abdullah.Alhaif@Tuwaiq.edu.sa>
2021-04-21 15:37:04 +0300

    char range problem has been solved!

---
+++ Char range/char-range.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Class0204_6
+{
+    class Program
+    {
+        public static bool insideTheRange(char lookingForChar, char firstRange, char secondRange)
+        {
+            int firstCount = 0;
+            int secondCount = 0;
+            string alpha = "abcdefghijklmnopqrstuvwxyz";
+            //input give me the first char
+            for (int i = 0; i < alpha.Length; i++)
+            {
+                if (alpha[i] == firstRange)
+                {
+                    firstCount = i;
+                }
+                if (alpha[i] == secondRange)
+                {
+                    secondCount = i;
+                }
+            }
+            string subS = alpha.Substring(firstCount, secondCount - firstCount);
+            foreach (var item in subS)
+            {
+                if (subS.Contains(lookingForChar))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(insideTheRange('y','x','z'));
+        }
+    }
+}
