10140aa18ca1c1fa252c315d2fd88381e413f64c
mohammed-tu <82476026+mohammed-tu@users.noreply.github.com>
2021-04-21 16:14:31 +0300

    Add files via upload
    from Mohammed

---
+++ hidden-folder/hidden-file.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp_4_211
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // التحدي الأول
+            Console.WriteLine("Enter your Folder :");
+            string input = Console.ReadLine();
+            if (input.StartsWith('.'))
+            {
+                Console.WriteLine("Hidden folder : your folder ( {0} )", input);
+
+            }
+            else
+            {
+                Console.WriteLine("not Hidden : your folder  ( {0} )", input);
+            }
+        }
+    }
+}
