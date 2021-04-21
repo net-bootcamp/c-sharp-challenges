f8a12d3d98616004860865a0e44fd98e3020a2a4
mohammed-tu <82476026+mohammed-tu@users.noreply.github.com>
2021-04-21 16:19:44 +0300

    from Mohammed

---
+++ check-exist-number/exist-number.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp_4_211
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // التحدي الثاني
+
+            string text = "hello mohammed"; // output = there are no number
+            string check = "0123456789";
+            bool status = false;
+
+            foreach (var item in check)
+            {
+                if (text.Contains(item))
+                {
+                    status = true;
+                }
+            }
+
+            if (status)
+                Console.WriteLine("there are number");
+            else
+                Console.WriteLine("there are no number");
+
+        }
+    }
+}
