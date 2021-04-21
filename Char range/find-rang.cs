27c2775a987a5ce04a0a640d1a68f4b60d53013d
HussainAlghamdi <82505977+HussainAlghamdi@users.noreply.github.com>
2021-04-21 15:50:17 +0300

    Add files via upload

---
+++ Char range/find-rang.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace find_rang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string m = "abcdefghijklmnopqrstuvwxyz";
+
+            Console.WriteLine(rangreturn('h', 'a', 'z'));
+        }
+
+        static bool rangreturn(Char x, char y, char z)
+        {
+            if (x >= y && x <= z)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
