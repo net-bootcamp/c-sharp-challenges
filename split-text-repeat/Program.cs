04d587cc246aadf0fe8c87a220b58c7c75f6a9dc
dev <dev@test.com>
2021-04-22 14:06:21 +0300

    split text repeated

---
+++ split-text-repeat/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace csession
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string text = "hello,world this is.me";
+            char[] c = { ' ', ',', '.' };
+            string[] MyStrings = text.Split(c);
+
+            foreach (var item in MyStrings)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+
+    }
+}
