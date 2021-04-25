617bf92dc181e7f6b60efab988dcd36ede1b22b8
SamirahAlhusayni <samirah.alhusayni@tuwaiq.edu.sa>
2021-04-25 13:33:56 +0300

    CupSwap Challenge

---
+++ CupSwapChallenge/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace cupSwap
+{
+    class Program
+    {
+        public static string CupSwap(string [] swap)
+        {
+            string position="B";
+      
+            
+            for (int i = 0; i < swap.Length ; i++)
+            {
+                if (swap[i].Contains (position ) )
+                {
+                    position = swap[i].Replace(position, "");
+
+                }
+            }
+            return position;
+        
+        }
+
+        static void Main(string[] args)
+        {
+            string[] swaps= { "AB", "CA", "CA", "AC" };
+            Console.WriteLine($"Current cup contains the ball is : {CupSwap(swaps)} ");
+        }
+    }
+}
---
+++ CupSwapChallenge/README.md
@@ -0,0 +1,10 @@
+#CupSwapping Challenge:
+
+How to find the current position of ball after sequence of swapping 
+As a hint you can use:
+1. Contans() method
+2. Replace method
+
+
+
+
